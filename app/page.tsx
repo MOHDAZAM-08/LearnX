5cb00cd7478b84e6c942ae0327bfc5bcabf9a02d
Azam <azamkhan6169@gmail.com>
2024-08-27 02:30:07 +0530

    home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import { url } from "inspector";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-   <h1>hello</h1>
+     <div className="flex items-center justify-center min-h-screen bg-gray-100 "
+      style={{
+        backgroundImage: 'url(/bg.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}>
+      <Link href="/home"> 
+      <h1 className="text-3xl font-bold text-white">Home</h1>
+      </Link>
+
+    </div>
     </main>
   );
 }
